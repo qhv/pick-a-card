3f785c202034585b552566c94df6d6b27fe9bfff
Alex Dobrovolski <triiq@mail.ru>
2020-10-10 22:23:24 +0300

    Change suits view

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
 ranks = %w(2 3 4 5 6 7 8 9 10 J Q K A)
-suits = %w(Diamonds Hearts Spades Clubs)
+suits = %w(♦ ♠ ♥ ♣)
 
-puts "#{ranks.sample} of #{suits.sample}"
+puts "#{ranks.sample}#{suits.sample}"
